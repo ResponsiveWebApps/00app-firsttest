16996dc98fe958b93107ca792e3b9edaa3acc64d
Don Roche <donrochemedia@gmail.com>
2021-01-26 21:30:38 +0000

    Exercise 2 done

--- w3.py
+++ w3.py
@@ -24,6 +24,30 @@ print(result == 11)
     For example if someone put in 8, then it would print 'eight'.
     Hint: Use if, elif and else statements.
 """
+num = int(input("Type in a number 1-10: "))
+
+if num == 1:
+    print("One")
+elif num == 2:
+    print("Two")
+elif num == 3:
+    print("Threw")
+elif num == 4:
+    print("Four")
+elif num == 5:
+    print("Five")
+elif num == 6:
+    print("Six")
+elif num == 7:
+    print("Seven")
+elif num == 8:
+    print("Eight")
+elif num == 9:
+    print("Nine")
+elif num == 10:
+    print("Ten")
+
+
 """
     =========== Exercise 3 =============
     Before running the code below try to
